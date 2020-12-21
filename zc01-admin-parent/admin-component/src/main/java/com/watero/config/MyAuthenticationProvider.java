7a5b6370ffafbb97346cc65b6ec3f9ba91d65931
Yang <897238156@qq.com>
2020-12-21 20:21:10 +0800

    1

---
+++ zc01-admin-parent/admin-component/src/main/java/com/watero/config/MyAuthenticationProvider.java
@@ -0,0 +1,25 @@
+package com.watero.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.Serializable;
+public class MyAuthenticationProvider extends AbstractUserDetailsAuthenticationProvider {
+    @Override
+    protected void additionalAuthenticationChecks(UserDetails userDetails, UsernamePasswordAuthenticationToken authentication) throws AuthenticationException {
+
+    }
+
+    @Override
+    protected UserDetails retrieveUser(String username, UsernamePasswordAuthenticationToken authentication) throws AuthenticationException {
+        return null;
+    }
+}
--- zc01-admin-parent/admin-component/src/main/java/com/watero/config/SecurityConfig.java
+++ zc01-admin-parent/admin-component/src/main/java/com/watero/config/SecurityConfig.java
@@ -1,69 +1,66 @@
 package com.watero.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true) // 启用方法安全设置
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private static final String KEY = "edward";
-
-    @Autowired
-    private UserDetailsService userDetailsService;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();   // 使用 BCrypt 加密
-    }
-
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userDetailsService);
-        authenticationProvider.setPasswordEncoder(passwordEncoder); // 设置密码加密方式
-        return authenticationProvider;
-    }
-
-    /**
-     * 自定义配置
-     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/css/**", "/js/**", "/fonts/**", "/index").permitAll() // 都可以访问
-                .antMatchers("/h2-console/**").permitAll() // 都可以访问
-                .antMatchers("/admin/**").hasRole("ADMIN") // 需要相应的角色才能访问
+        http.authorizeRequests()//开启登录配置
+                .antMatchers("/admin/**").hasRole("admin")//访问/admin/**下的url需要admin这个角色
+                .antMatchers("/hello").permitAll()//访问/hello,不需要权限控制
+                .anyRequest().authenticated()//其他都需要授权
                 .and()
-                .formLogin()   //基于 Form 表单登录验证
-                .loginPage("/login").failureUrl("/login-error") // 自定义登录界面
-                .and().rememberMe().key(KEY) // 启用 remember me
-                .and().exceptionHandling().accessDeniedPage("/403");  // 处理异常，拒绝访问就重定向到 403 页面
-        http.csrf().ignoringAntMatchers("/h2-console/**"); // 禁用 H2 控制台的 CSRF 防护
-        http.headers().frameOptions().sameOrigin(); // 允许来自同一来源的H2 控制台的请求
+                .formLogin()//定义登录页面，未登录时，访问一个需要登录之后才能访问的接口，会自动跳转到该页面
+                .loginPage("/index.html")//自定义登陆页面
+                .loginProcessingUrl("/admin/login")//自定义的登陆处理接口
+                //登陆成功的处理器
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        response.setContentType("application/json;charset=utf-8");
+                        PrintWriter out = response.getWriter();
+                        out.write("login success");
+                        out.flush();
+                    }
+                })
+                .permitAll()//和表单登录相关的接口统统都直接通过
+                .and()
+                .logout()//注销登录
+                .logoutUrl("/admin/logout")
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        response.setContentType("application/json;charset=utf-8");
+                        PrintWriter out = response.getWriter();
+                        out.write("logout success");
+                        out.flush();
+                    }
+                })
+                .permitAll()//和注销登陆相关的接口统统都直接通过
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable();//关闭csrf
     }
 
-    /**
-     * 认证信息管理
-     *
-     * @param auth
-     * @throws Exception
-     */
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
-        auth.authenticationProvider(authenticationProvider());
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/resources/**");//放行静态资源
     }
 }
